f5d0c4b0378eab056cb300dce4ae7acdde592c72
leahnjambi <njambileah07@gmail.com>
2026-08-13 15:37:45 +0300

    feat : created a service component to be reused  when creating  services

---
+++ src/services/service.tsx
@@ -0,0 +1,23 @@
+import '../App.css'
+type Service ={
+    image : string;
+    title : string;
+    description : string;
+};
+function OurService({
+    image,
+    title,
+    description
+}: Service){
+    return(
+        <div className="service-container">
+            <div className="service-header">
+                <img src={image} alt={title} 
+                />
+                <h1>{title}</h1>
+            </div>
+            <div>{description}</div>
+        </div>
+    )
+}
+export default OurService;
