906e8ccac34299b7ee9e2211ad188b82f63a43c6
leahnjambi <njambileah07@gmail.com>
2026-08-14 16:55:29 +0300

    refractor : remove the component and store them in an arrary to implement usestate function  and .map

--- src/experience/ourExperienceSection.tsx
+++ src/experience/ourExperienceSection.tsx
@@ -1,39 +1,65 @@
 import OurExperience from "./experience";
 import union from '../assets/Union.png'
 import '../App.css'
+
+const Experience = [
+    {
+        id : 1,
+        image : union,
+        title : "jifjrkejjfbuhyr"
+    },
+    {
+        id : 2,
+        image : union,
+        title : "jifjrkejjfbuhyr"
+    },
+    {
+        id : 3,
+        image : union,
+        title : "jifjrkejjfbuhyr"
+    },
+    {
+        id : 4,
+        image : union,
+        title : "jifjrkejjfbuhyr"
+    },
+    {
+        id : 5,
+        image : union,
+        title : "jifjrkejjfbuhyr"
+    },
+    {
+        id : 6,
+        image :union,
+        title :"jifjrkejjfbuhyr"
+    },
+    {
+        id : 7,
+        image : union,
+        title :"jifjrkejjfbuhyr"
+    },
+    {
+        id : 8,
+        image : union,
+        title :"jifjrkejjfbuhyr"
+    }
+
+
+];
 function OurExpertSection(){
     return(
         <section className="experience-section">
             <h1>Experience</h1>
             <div className="experience-container">
-                <OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                />
-                <OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                />
-                <OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                /><OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                /><OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                /><OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                /><OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                /><OurExperience
-                image={union}
-                title="jifjrkejjfbuhyr"
-                />
-
+                {Experience.map((experience) => (
+                    <OurExperience
+                    key = {experience.id}
+                    id = {experience.id}
+                    image={experience.image}
+                    title={experience.title}
+                    />
+                ))}
+                
             </div>
         </section>
     )
