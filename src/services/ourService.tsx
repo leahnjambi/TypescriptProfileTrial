30dff4e5aaafcff77c89fe30f82dd4a08017ff5d
leahnjambi <njambileah07@gmail.com>
2026-08-14 16:56:24 +0300

    refractor : remove the service  component and store them in an arrary to implement usestate function  and .map

--- src/services/ourService.tsx
+++ src/services/ourService.tsx
@@ -1,30 +1,51 @@
 import OurService from "./service"
 import logo from '../assets/Union.png'
+
+const Service = [
+    {
+        id : 1,
+        image:logo,
+        title :"ghdhoireigf",
+        description :"gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
+    },
+    {
+        id : 2,
+        image : logo,
+        title : "ghdhoireigf",
+        description :"gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
+    },
+    {
+        id : 3,
+        image : logo,
+        title :"ghdhoireigf",
+        description : "gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
+    },
+    {
+        id : 4,
+        image : logo,
+        title :"ghdhoireigf",
+        description:"gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
+    },
+];
+
+
 function OurServices(){
     return(
         <section className="ourService-section">
             <h1>Our Service</h1>
             <div className="ourservice-container">
-                <OurService
-                image={logo}
-                title="ghdhoireigf"
-                description="gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
-                />
-                <OurService
-                image={logo}
-                title="ghdhoireigf"
-                description="gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
-                />
-                <OurService
-                image={logo}
-                title="ghdhoireigf"
-                description="gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
-                />
-                <OurService
-                image={logo}
-                title="ghdhoireigf"
-                description="gdkdbsdjdjkjekiweuehjjdhgdbcsnmcdhhd"
-                />
+                {Service.map((service) => (
+                    <OurService
+                     key={service.id}
+                     id = {service.id}
+                     image={service.image}
+                     title={service.title}
+                     description={service.description}
+
+                    />
+
+                )
+            )}
 
             </div>
 
