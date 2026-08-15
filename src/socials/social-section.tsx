b95ca857b80472f370d7bcbb2c8aa3cad5d01baa
leahnjambi <njambileah07@gmail.com>
2026-08-15 12:42:56 +0300

    feat : add a social section to display the social  icons and links

---
+++ src/socials/social-section.tsx
@@ -0,0 +1,23 @@
+import Social from "./social";
+import union from "../assets/Union.png"
+
+
+function Mysocial(){
+    return(
+        <section className="social-section">
+            <div className="social-container">
+            <Social
+            logo  ={union}
+            title = {"hjhhgyiufsu"}
+            socialicon={union}
+            icontitle="jduhtf"
+            link = {"http/icon"}
+
+            />
+            </div>
+
+        </section>
+
+    )
+}
+export default Mysocial;
