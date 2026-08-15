d33e8235da1088bca7a320e1bdc0396ac1091e8e
leahnjambi <njambileah07@gmail.com>
2026-08-15 12:39:50 +0300

    feat : add appropriate  icons for the experience languages

--- src/experience/ourExperienceSection.tsx
+++ src/experience/ourExperienceSection.tsx
@@ -1,36 +1,42 @@
 import OurExperience from "./experience";
 import union from '../assets/Union.png'
 import '../App.css'
+import python from '../assets/social-icons/python.jpg'
+import htmlicon from '../assets/social-icons/html.png'
+import cssicon from '../assets/social-icons/css.png'
+import cicon from '../assets/social-icons/c++.png'
+import myreacticon from '../assets/social-icons/react.png'
+import mytypescript from '../assets/social-icons/typescript.png'
 
 const Experience = [
     {
         id : 1,
-        image : union,
+        image : python,
         title : "jifjrkejjfbuhyr"
     },
     {
         id : 2,
-        image : union,
+        image : cicon,
         title : "jifjrkejjfbuhyr"
     },
     {
         id : 3,
-        image : union,
+        image : myreacticon,
         title : "jifjrkejjfbuhyr"
     },
     {
         id : 4,
-        image : union,
+        image : mytypescript,
         title : "jifjrkejjfbuhyr"
     },
     {
         id : 5,
-        image : union,
+        image : htmlicon,
         title : "jifjrkejjfbuhyr"
     },
     {
         id : 6,
-        image :union,
+        image :cssicon,
         title :"jifjrkejjfbuhyr"
     },
     {
